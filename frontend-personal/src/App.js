a6cee7d48f0e6a73df0fd2de404873aa9a9abf5c
OzzY <ewagne02@calpoly.edu>
2022-12-12 22:36:42 -0800

    main page design

--- frontend-personal/src/App.js
+++ frontend-personal/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 //material ui drawer imports
 import Box from '@mui/material/Box';
 import {Drawer, Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from '@mui/material';
+import HomeIcon from '@mui/icons-material/HomeOutlined';
 import AboutIcon from '@mui/icons-material/EmojiPeopleOutlined';
 import RamblingIcon from '@mui/icons-material/MoreHoriz';
 import ResumeIcon from '@mui/icons-material/HandshakeOutlined';
 import ProjectsIcon from '@mui/icons-material/Code';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 import { createTheme } from '@mui/material/styles';
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
@@ -32,52 +34,76 @@ function App() {
     setNavBar(open);
   };
 
+  //overrides material ui text theme
   const theme = createTheme({
     typography: {
       fontFamily: ['Courier New', 'Courier', 'monospace'].join(','),
-      color: 'darkolivegreen',
+      fontSize: 'medium',
+      fontWeight: 600,
+      color: '#bb5347',
       }
     })
-
-  const navList = ['/projects', '/about', '/rambling'];
+  
+  // variables to be used for drawer items
+  const navList = ['/', '/projects', '/about', '/rambling'];
+  const GitHub = "https://github.com/EthanRWagner";
+  const LinkedIn = "https://www.linkedin.com/in/ethanrwagner/";
 
   // navBar element / drawer
   const drawer = (
     <Box
-      sx={{ width: 250 }}
+      sx={{ width: 250, height: '100%' }}
       role="presentation"
       onClick={toggleNavBar(false)}
       onKeyDown={toggleNavBar(false)}
       className="navBar-box"
     >
       <List>
-        {['projects', 'about', 'rambling'].map((text, index) => (
-          <Link to={navList[index]}>
+        {['home', 'projects', 'about', 'rambling'].map((text, index) => (
+          <Link to={navList[index]} style={{ textDecoration: 'none' }}>
             <ListItem key={text} disablePadding>
               <ListItemButton>
                 <ListItemIcon
                   sx={{
-                    color: 'darkolivegreen',
+                    color: '#bb5347',
                   }}>
-                  {(index === 0) ? <ProjectsIcon /> :
-                  (index === 1) ? <AboutIcon /> : <RamblingIcon />}
+                  {(index === 0) ? <HomeIcon /> :
+                  (index === 1) ? <ProjectsIcon /> :
+                  (index === 2) ? <AboutIcon /> : <RamblingIcon />}
                 </ListItemIcon>
                 <ListItemText primaryTypographyProps={theme} primary={text} />
               </ListItemButton>
             </ListItem>
           </Link>
         ))}
-        <ListItem onClick={() => window.open("https://github.com/EthanRWagner", '_blank')} key={"github"} disablePadding>
+        <ListItem onClick={() => window.open(GitHub, '_blank')} key={"github"} disablePadding>
           <ListItemButton>
-            <ListItemIcon>
+            <ListItemIcon
+              sx={{
+                color: '#bb5347',
+              }}>
               <GitHubIcon />
             </ListItemIcon>
             <ListItemText primaryTypographyProps={theme} primary={"github"} />
           </ListItemButton>
         </ListItem>
+        <ListItem onClick={() => window.open(LinkedIn, '_blank')} key={"linkedIn"} disablePadding>
+          <ListItemButton>
+            <ListItemIcon
+              sx={{
+                color: '#bb5347',
+              }}>
+              <LinkedInIcon />
+            </ListItemIcon>
+            <ListItemText primaryTypographyProps={theme} primary={"linkedIn"} />
+          </ListItemButton>
+        </ListItem>
         <ListItem onClick={() => window.open(Resume, '_blank')} key={"resume"} disablePadding>
           <ListItemButton>
-            <ListItemIcon>
+            <ListItemIcon
+              sx={{
+                color: '#bb5347',
+              }}>
               <ResumeIcon />
             </ListItemIcon>
             <ListItemText primaryTypographyProps={theme} primary={"resume"} />
@@ -93,7 +119,7 @@ function App() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
       </head>
 
-      <body>
+      <body className='main-page-background'>
         <Router>
           <div>
             <div>
@@ -108,23 +134,26 @@ function App() {
                   {drawer}
                 </Drawer>
             </div>
-          <Routes>
-            <Route path="/">
-              Home
-            </Route>
+            <Routes>
+              <Route path="/">
+                Home
+              </Route>
 
-            <Route path="/projects">
-              Projects
-            </Route>
+              <Route path="/projects">
+                Projects
+              </Route>
 
-            <Route path="/about">
-              About
-            </Route>
+              <Route path="/about">
+                About
+              </Route>
 
-            <Route path="/rambling">
-              Rambling
-            </Route>
-          </Routes>
+              <Route path="/rambling">
+                Rambling
+              </Route>
+            </Routes>
+          </div>
+          <div className='content-container'>
+              <large className='title-text'>ethan wagner</large>
           </div>
         </Router>
       </body>
